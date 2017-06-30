e69859ee30fdbe3f69d5d83a818288a0049c64a8
Ryan J. Geyer <me@ryangeyer.com>
2017-06-30 16:42:45 -0700

    Token still not being set?

--- support_scripts/jenkinsBuildScript.sh
+++ support_scripts/jenkinsBuildScript.sh
@@ -7,7 +7,7 @@ rsc_cmd="./rsc -h ${RS_HOST} -a ${RS_ACCOUNT} -r ${RS_TOKEN}"
 right_script_id=$(${rsc_cmd} --x1 .id cm15 index right_scripts 'filter[]=name==DevOps Demo Webpage Installer')
 if [ -z "$right_script_id" ]; then
   # No RightScript, so let's upload it.
-  ./right_st rightscript upload DevOps_Demo_Webpage_installer.sh
+  ./right_st rightscript upload support_scripts/DevOps_Demo_Webpage_installer.sh
   right_script_id=$(${rsc_cmd} --x1 .id cm15 index right_scripts 'filter[]=name==DevOps Demo Webpage Installer')
 fi
 right_script_href="/api/right_scripts/$right_script_id"
