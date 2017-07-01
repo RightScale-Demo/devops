4b2de35719f65b28e28559f0bd46742211780801
Ryan J. Geyer <me@ryangeyer.com>
2017-06-30 17:26:57 -0700

    Updated secure travis value, allow the deploy script to actually exit with non zero on failure

--- support_scripts/jenkinsBuildScript.sh
+++ support_scripts/jenkinsBuildScript.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+set -e
 export RIGHT_ST_LOGIN_ACCOUNT_ID=$RS_ACCOUNT
 export RIGHT_ST_LOGIN_ACCOUNT_HOST=$RS_HOST
 export RIGHT_ST_LOGIN_ACCOUNT_REFRESH_TOKEN=$RS_TOKEN
@@ -7,7 +9,7 @@ rsc_cmd="./rsc -h ${RS_HOST} -a ${RS_ACCOUNT} -r ${RS_TOKEN}"
 right_script_id=$(${rsc_cmd} --x1 .id cm15 index right_scripts 'filter[]=name==DevOps Demo Webpage Installer')
 if [ -z "$right_script_id" ]; then
   # No RightScript, so let's upload it.
-  ./right_st rightscript upload support_scripts/DevOps_Demo_Webpage_installer.sh
+  ./right_st rightscript upload support_scripts/DevOps_Demo_Webpage_Installer.sh
   right_script_id=$(${rsc_cmd} --x1 .id cm15 index right_scripts 'filter[]=name==DevOps Demo Webpage Installer')
 fi
 right_script_href="/api/right_scripts/$right_script_id"
